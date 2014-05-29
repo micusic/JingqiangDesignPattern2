56d12aa5805ccfd29b871a3634c08099caa046d3
tklae <github@klaesener.net>
2014-05-29 09:53:47 +0530

    Remove unused separator from Cocktail

--- src/Cocktail.java
+++ src/Cocktail.java
@@ -1,8 +1,6 @@
 import java.util.List;
 
 public abstract class Cocktail {
-    public static String separator = "|";
-
     public abstract double getPrice();
     public abstract List<String> getIngredients();
 }
